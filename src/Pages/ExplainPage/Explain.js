51803f02e0f0e80fbfae7e970d82187079173397
You Hyun sang <pycandyq@naver.com>
2023-08-17 02:40:32 +0900

    [ADD]웹소개 페이지 SCSS파일 추가

--- src/Pages/ExplainPage/Explain.js
+++ src/Pages/ExplainPage/Explain.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import Card from '../../UI/Card'
+import React from 'react';
+import Card from '../../UI/Card';
+import './Explain.scss';
 function Explain() {
   return (
     <div>
