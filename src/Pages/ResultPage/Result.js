7b781da6f45a67007da8057c8de7d2b96ae9f5f9
You Hyun sang <pycandyq@naver.com>
2023-08-18 22:51:21 +0900

    수정수정수정

--- src/Pages/ResultPage/Result.js
+++ src/Pages/ResultPage/Result.js
@@ -61,7 +61,7 @@ function Result() {
                 navigate('/rank');
                 
             } else {
-                console.error('Failed to insert data: ', error);
+                console.error('Failed to insert data:' );
                 
             }
         } catch (error) {
